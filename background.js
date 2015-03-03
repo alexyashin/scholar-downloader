11a44232fcd70e12a9b3c895327d261c81d70877
Alexey Yashin <tylz@yandex.ru>
2015-03-03 22:48:12 +0300

    Article limit added. Page with no links fixed

--- background.js
+++ background.js
@@ -1,46 +1,61 @@
 var runPlugin = false;
+var ARTICLE_COUNT = 5;
+var CURRENT_COUNT = 0;
+
 chrome.browserAction.onClicked.addListener(function(tab) {
     runPlugin = !runPlugin;
     if (runPlugin) {
         runPlugin = true;
         console.log("Run scholar downloader");
-        chrome.browserAction.setIcon({path: "stop-icon.png", tabId:tab.id});
-        chrome.tabs.executeScript(null, {file: "content_script.js"});
-    }
-    else{
+        chrome.browserAction.setIcon({
+            path: "stop-icon.png",
+            tabId: tab.id
+        });
+        chrome.tabs.executeScript(null, {
+            file: "content_script.js"
+        });
+        CURRENT_COUNT = 0;
+    } else {
         runPlugin = false;
         console.log("Stop scholar downloader");
 
     }
 });
 
-chrome.extension.onConnect.addListener(function (port) {
+chrome.extension.onConnect.addListener(function(port) {
     if (!runPlugin)
         return;
-    port.onMessage.addListener(function (msg) {
+    port.onMessage.addListener(function(msg) {
         if (msg.content_script == "ready") {
-            port.postMessage({extension: "getLinks"});
-        }
-        else if (msg.content_script == "links") {
+            port.postMessage({
+                extension: "getLinks"
+            });
+        } else if (msg.content_script == "links") {
             var links = msg.links;
             if (links != null) {
                 downloadLinks(links);
             }
-            port.postMessage({extension: "goToNext"});
-        }
-        else if (msg.content_script == "iAmGoing") {
+            port.postMessage({
+                extension: "goToNext"
+            });
+        } else if (msg.content_script == "iAmGoing") {
             //port.postMessage({extension: "getLinks"});
         }
     });
 });
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (!runPlugin){
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (!runPlugin) {
         return;
     }
     if (/.*scholar\.google\..*\?/.test(tab.url) && changeInfo.status === "complete") {
-        chrome.browserAction.setIcon({path: "stop-icon.png", tabId:tab.id});
-        chrome.tabs.executeScript(null, {file: "content_script.js"});
+        chrome.browserAction.setIcon({
+            path: "stop-icon.png",
+            tabId: tab.id
+        });
+        chrome.tabs.executeScript(null, {
+            file: "content_script.js"
+        });
     }
 });
 
@@ -48,6 +63,13 @@ function downloadLinks(links) {
     for (var i = 0; i < links.length; i++) {
         var link = links[i];
         var fileName = link.split('/').pop();
-        chrome.downloads.download({url: link});
+        chrome.downloads.download({
+            url: link
+        });
+        CURRENT_COUNT += 1;
+        if (CURRENT_COUNT >= ARTICLE_COUNT) {
+            runPlugin = false;
+            break;
+        }
     }
 }
--- content_script.js
+++ content_script.js
@@ -1,15 +1,24 @@
-console.log("CONTENT SCRIPT "+document.URL);
+console.log("CONTENT SCRIPT " + document.URL);
 
-var port = chrome.extension.connect({name: "linkGetter"});
-port.postMessage({content_script: "ready"});
+var port = chrome.extension.connect({
+    name: "linkGetter"
+});
+port.postMessage({
+    content_script: "ready"
+});
 port.onMessage.addListener(function(msg) {
-    if (msg.extension == "getLinks"){
-        port.postMessage({content_script: "links", links: getLinks(), url: document.URL});
-    }
-    else if (msg.extension == "goToNext"){
+    if (msg.extension == "getLinks") {
+        port.postMessage({
+            content_script: "links",
+            links: getLinks(),
+            url: document.URL
+        });
+    } else if (msg.extension == "goToNext") {
         sleep(3000);
         clickNext();
-        port.postMessage({content_script: "iAmGoing"});
+        port.postMessage({
+            content_script: "iAmGoing"
+        });
     }
 });
 
@@ -17,7 +26,9 @@ function getLinks() {
     var content = document.documentElement.innerHTML;
     var regEx = /http:[^"]*\.pdf/gi;
     links = content.match(regEx);
-    links = links.filter( function( item, index, inputArray ) {
+    if (links == null) 
+        return null;
+    links = links.filter(function(item, index, inputArray) {
         return inputArray.indexOf(item) == index;
     });
     return links;
@@ -29,5 +40,5 @@ function clickNext() {
 
 function sleep(ms) {
     ms += new Date().getTime();
-    while (new Date() < ms){}
+    while (new Date() < ms) {}
 }
